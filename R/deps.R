a1ff36aecde87aa969551ae076763b90ef7e96f7
Gábor Csárdi <csardi.gabor@gmail.com>
2017-06-26 22:03:29 +0100

    Work around a dependsOnPkgs bug

--- R/deps.R
+++ R/deps.R
@@ -11,10 +11,14 @@ cran_revdeps <- function(package, dependencies, bioc) {
     c("CRAN-cloud" = "https://cloud.r-project.org")
   )
 
-  dependsOnPkgs(
+  res <- dependsOnPkgs(
     package,
     recursive = FALSE,
     installed = available_packages(repos = repos),
     dependencies = dependencies
   )
+
+  ## dependsOnPkgs sometimes return and empty string, not sure why,
+  ## but we just remove it for now
+  setdiff(res, "")
 }
