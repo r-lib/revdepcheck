c6264835bb65bde3d66454d4fd24c0d9069200c5
hadley <h.wickham@gmail.com>
2017-06-28 17:57:04 -0500

    Beginning work on markdown outputs

---
+++ R/library.R
@@ -0,0 +1,25 @@
+library_compare <- function(pkg) {
+  lib_new <- library_meta(check_dir(pkg, "new"))
+  lib_old <- library_meta(check_dir(pkg, "old"))
+
+  lib_cmp <- merge(
+    data.frame(package = lib_old$Package, old = lib_old$Version, stringsAsFactors = FALSE),
+    data.frame(package = lib_new$Package, new = lib_new$Version, stringsAsFactors = FALSE),
+    all = TRUE
+  )
+
+  same <- function(x, y) (is.na(x) == is.na(y)) & (x == y)
+  lib_cmp[["Δ"]] <- ifelse(same(lib_cmp$new, lib_cmp$old), "", "*")
+
+  # Move tested package to top
+  pkgname <- get_package_name(pkg)
+
+  idx <- which(lib_cmp$package == pkgname)
+  lib_cmp[union(idx, seq_len(nrow(lib_cmp))), , drop = FALSE]
+}
+
+library_meta <- function(libpath) {
+  lib <- installed.packages(libpath)
+  rownames(lib) <- NULL
+  as.data.frame(lib, stringsAsFactors = FALSE)
+}
--- R/report.R
+++ R/report.R
@@ -3,33 +3,93 @@
 #' @family revdep check functions
 #' @export
 #' @importFrom crayon black red yellow green
+#' @importFrom sessioninfo platform_info
 
-revdep_report <- function(pkg = ".",
-                          revdeps = NULL) {
+revdep_report_summary <- function(pkg = ".", revdeps = NULL) {
+  cat_header("Platform")
+  cat_line()
+  platform <- platform_info()
+  platform_df <- data.frame(field = names(platform), value = unlist(platform))
+  cat_kable(platform_df)
 
-  n_todo <- length(db_todo(pkg))
-  if (n_todo > 0) {
-    message(black("* ", yellow(as.character(n_todo)), " packages left to check"))
-  }
+  cat_header("Libraries")
+  cat_line()
+  cat_kable(library_compare(pkg))
 
-  if (is.null(revdeps)) {
-    revdeps <- db_list(pkg)
-  }
+  cat_header("Revdeps")
+  cat_line()
 
   packages <- revdep_results(pkg, revdeps)
-  broken <- vapply(packages, is_broken, integer(1))
+  broken <- vapply(packages, is_broken, logical(1))
 
+  n_todo <- length(db_todo(pkg))
   n_ok <- sum(!broken)
-  if (n_ok > 0) {
-    message(black("* ", green(as.character(n_ok)), " packages with no new failures"))
-  }
-
   n_broken <- sum(broken)
-  if (n_broken > 0) {
-    message(black("* ", red(as.character(n_broken)), "packages with new failures"))
-    details <- lapply(revdeps[broken], revdep_details, pkg = pkg)
-    print(details)
+
+  cat_bullet("BROKEN: ", red(as.character(n_broken)))
+  cat_bullet("OK: ", green(as.character(n_ok)))
+  if (n_todo > 0) {
+    cat_bullet("TODO: ", yellow(as.character(n_todo)))
   }
+  cat_line()
+  print(packages)
 
   invisible()
 }
+
+revdep_report_problems <- function(pkg = ".", revdeps = NULL) {
+  packages <- revdep_results(pkg, revdeps)
+  broken <- vapply(packages, is_broken, logical(1))
+
+  lapply(packages[broken], failure_details)
+
+  invisible()
+}
+
+failure_details <- function(x) {
+  old <- x$old[[1]]
+  cat_header(old$package, " (", old$version, ")")
+  cat_line()
+
+  new <- x$cmp[x$cmp$which == "new", , drop = FALSE]
+  cat(format_details_bullets(new$output), sep = "")
+
+  invisible()
+}
+
+format_details_bullets <- function(x, max_lines = 20) {
+  vapply(x, format_details_bullet, max_lines = max_lines, FUN.VALUE = character(1))
+}
+
+format_details_bullet <- function(x, max_lines = 20) {
+  lines <- strsplit(x, "\n")[[1]]
+
+  title <- trimws(lines[[1]])
+  details <- lines[-1]
+
+  n <- length(details)
+  if (n > max_lines) {
+    details <- c("...", details[(n - max_lines):n])
+  }
+  details <- c("```", details, "```")
+
+  pad <- strrep(" ", 4)
+  paste0(
+    "*   ", red(title), "\n",
+    paste0(pad, details, "\n", collapse = ""),
+    "\n"
+  )
+}
+
+#' @importFrom knitr kable
+#' @importFrom crayon bold
+
+cat_line <- function(...) cat(..., "\n", sep = "")
+cat_kable <- function(x, ...) {
+  cat(kable(x, row.names = FALSE), sep = "\n")
+  cat_line()
+}
+cat_header <- function(..., level = 1) {
+  cat(bold(paste0(strrep("#", level), " ", ...)), "\n", sep = "")
+}
+cat_bullet <- function(...) cat_line("* ", ...)
