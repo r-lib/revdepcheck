b6eb2668650581e818681d5e0d98f8fc666a03ae
Gábor Csárdi <csardi.gabor@gmail.com>
2025-10-03 16:18:41 +0200

    Cloud checks: use R 4.5.1 by default

--- R/cloud.R
+++ R/cloud.R
@@ -204,7 +204,7 @@ cloud_check <- function(
   tarball = NULL,
   revdep_packages = NULL,
   extra_revdeps = NULL,
-  r_version = "4.4.0",
+  r_version = "4.5.1",
   check_args = "--no-manual",
   bioc = FALSE
 ) {
